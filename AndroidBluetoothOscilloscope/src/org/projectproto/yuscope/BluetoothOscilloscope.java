1682c3187c2880bac97506b91cee4c990afe488d
yus <yus.projectproto@gmail.com>
2011-09-10 22:47:25 +0800

    completed BluetoothRfcommClient class

--- AndroidBluetoothOscilloscope/src/org/projectproto/yuscope/BluetoothOscilloscope.java
+++ AndroidBluetoothOscilloscope/src/org/projectproto/yuscope/BluetoothOscilloscope.java
@@ -4,6 +4,18 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class BluetoothOscilloscope extends Activity {
+	
+	// Message types sent from the BluetoothRfcommClient Handler
+    public static final int MESSAGE_STATE_CHANGE = 1;
+    public static final int MESSAGE_READ = 2;
+    public static final int MESSAGE_WRITE = 3;
+    public static final int MESSAGE_DEVICE_NAME = 4;
+    public static final int MESSAGE_TOAST = 5;
+    
+ // Key names received from the BluetoothRfcommClient Handler
+    public static final String DEVICE_NAME = "device_name";
+    public static final String TOAST = "toast";
+    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
