eb55e50bdd2c598d6b4d8c3b7903c82ca025016f
yus <yus.projectproto@gmail.com>
2011-09-11 10:58:11 +0800

    application can now connect to a remote bluetooth-spp device

--- AndroidBluetoothOscilloscope/src/org/projectproto/yuscope/BluetoothOscilloscope.java
+++ AndroidBluetoothOscilloscope/src/org/projectproto/yuscope/BluetoothOscilloscope.java
@@ -2,12 +2,16 @@ package org.projectproto.yuscope;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.view.Window;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class BluetoothOscilloscope extends Activity implements  Button.OnClickListener{
@@ -28,10 +32,15 @@ public class BluetoothOscilloscope extends Activity implements  Button.OnClickLi
     private static final int REQUEST_ENABLE_BT = 2;
     
     // Layout Views
+    private TextView mBTStatus;
     private Button mConnectButton;
     
+    // Name of the connected device
+    private String mConnectedDeviceName = null;
     // Local Bluetooth adapter
     private BluetoothAdapter mBluetoothAdapter = null;
+    // Member object for the RFCOMM services
+    private BluetoothRfcommClient mRfcommClient = null;
     
     
     /** Called when the activity is first created. */
@@ -65,13 +74,23 @@ public class BluetoothOscilloscope extends Activity implements  Button.OnClickLi
     	}
     	// Otherwise, setup the Oscillosope session
     	else{
-    		setupOscilloscope();
+    		if (mRfcommClient == null) setupOscilloscope();
     	}    	
     }
     
     @Override
     public synchronized void onResume(){
     	super.onResume();
+    	// Performing this check in onResume() covers the case in which BT was
+        // not enabled during onStart(), so we were paused to enable it...
+        // onResume() will be called when ACTION_REQUEST_ENABLE activity returns.
+        if (mRfcommClient != null) {
+            // Only if the state is STATE_NONE, do we know that we haven't started already
+            if (mRfcommClient.getState() == BluetoothRfcommClient.STATE_NONE) {
+              // Start the Bluetooth  RFCOMM services
+              mRfcommClient.start();
+            }
+        }
     }
     
     @Override
@@ -87,9 +106,13 @@ public class BluetoothOscilloscope extends Activity implements  Button.OnClickLi
     @Override
     public void onDestroy(){
     	super.onDestroy();
+    	// Stop the Bluetooth RFCOMM services
+        if (mRfcommClient != null) mRfcommClient.stop();
     }
     
     private void setupOscilloscope(){
+    	
+    	mBTStatus = (TextView) findViewById(R.id.txt_btstatus);
     	
     	mConnectButton = (Button) findViewById(R.id.button_connect);
     	mConnectButton.setOnClickListener(new OnClickListener(){
@@ -97,10 +120,73 @@ public class BluetoothOscilloscope extends Activity implements  Button.OnClickLi
 				BTConnect();
 			}    		
     	});
+    	
+    	// Initialize the BluetoothRfcommClient to perform bluetooth connections
+        mRfcommClient = new BluetoothRfcommClient(this, mHandler);
     }
     
     private void BTConnect(){
     	Intent serverIntent = new Intent(this, DeviceListActivity.class);
     	startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE);
+    }
+    
+    // The Handler that gets information back from the BluetoothRfcommClient
+    private final Handler mHandler = new Handler(){
+    	@Override
+        public void handleMessage(Message msg){
+    		switch (msg.what){
+    		case MESSAGE_STATE_CHANGE:
+    			switch (msg.arg1){
+    			case BluetoothRfcommClient.STATE_CONNECTED:
+    				mBTStatus.setText(R.string.title_connected_to);
+                    mBTStatus.append("\n" + mConnectedDeviceName);
+    				break;
+    			case BluetoothRfcommClient.STATE_CONNECTING:
+    				mBTStatus.setText(R.string.title_connecting);
+    				break;
+    			case BluetoothRfcommClient.STATE_NONE:
+    				mBTStatus.setText(R.string.title_not_connected);
+    				break;
+    			}
+    			break;
+    		case MESSAGE_DEVICE_NAME:
+    			// save the connected device's name
+                mConnectedDeviceName = msg.getData().getString(DEVICE_NAME);
+                Toast.makeText(getApplicationContext(), "Connected to "
+                        + mConnectedDeviceName, Toast.LENGTH_SHORT).show();
+    			break;
+    		case MESSAGE_TOAST:
+    			Toast.makeText(getApplicationContext(), msg.getData().getString(TOAST),
+                        Toast.LENGTH_SHORT).show();
+    			break;
+    		}
+    	}
+    };
+    
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+    	switch (requestCode) {
+    	case REQUEST_CONNECT_DEVICE:
+    		// When DeviceListActivity returns with a device to connect
+    		if (resultCode == Activity.RESULT_OK){
+    			// Get the device MAC address
+    			String address = data.getExtras().getString(DeviceListActivity.EXTRA_DEVICE_ADDRESS);
+    			// Get the BLuetoothDevice object
+                BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(address);
+                // Attempt to connect to the device
+                mRfcommClient.connect(device);
+    		}
+    		break;
+    	case REQUEST_ENABLE_BT:
+    		// When the request to enable Bluetooth returns
+            if (resultCode == Activity.RESULT_OK){
+            	// Bluetooth is now enabled, so set up the oscilloscope
+            	setupOscilloscope();
+            }else{
+            	// User did not enable Bluetooth or an error occured
+                Toast.makeText(this, R.string.bt_not_enabled_leaving, Toast.LENGTH_SHORT).show();
+                finish();
+            }
+    		break;
+    	}
     }
 }
