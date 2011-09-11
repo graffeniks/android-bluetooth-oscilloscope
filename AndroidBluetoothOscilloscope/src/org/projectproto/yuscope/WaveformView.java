9c7e0c120e26d33ee79b206acbe2f687b2ac2341
yus <yus.projectproto@gmail.com>
2011-09-11 13:58:38 +0800

    completed WaveformView class; initial plot for each channel

--- AndroidBluetoothOscilloscope/src/org/projectproto/yuscope/WaveformView.java
+++ AndroidBluetoothOscilloscope/src/org/projectproto/yuscope/WaveformView.java
@@ -1,3 +1,11 @@
+/***************************************
+ * 
+ * Android Bluetooth Oscilloscope
+ * yus	-	projectproto.blogspot.com
+ * September 2010
+ *  
+ ***************************************/
+
 package org.projectproto.yuscope;
 
 import android.content.Context;
@@ -10,12 +18,19 @@ import android.view.SurfaceView;
 
 public class WaveformView extends SurfaceView implements SurfaceHolder.Callback{
 	
+	// plot area size
+	private final static int WIDTH = 320;
+	private final static int HEIGHT = 240;
+	
+	private static int[] ch1_data = new int[WIDTH];
+	private static int[] ch2_data = new int[WIDTH];
+	private static int ch1_pos = 100; //HEIGHT/2;
+	private static int ch2_pos = 140; //HEIGHT/2;
+	
 	private WaveformPlotThread plot_thread;
 	
-	// plot area size
-	private final int WIDTH = 320;
-	private final int HEIGHT = 240;
-	
+	private Paint ch1_color = new Paint();
+	private Paint ch2_color = new Paint();
 	private Paint grid_paint = new Paint();
 	private Paint cross_paint = new Paint();
 	private Paint outline_paint = new Paint();
@@ -25,8 +40,16 @@ public class WaveformView extends SurfaceView implements SurfaceHolder.Callback{
 		
 		getHolder().addCallback(this);
 		
+		// initial values
+		for(int x=0; x<WIDTH; x++){
+			ch1_data[x] = ch1_pos;
+			ch2_data[x] = ch2_pos;
+		}
+		
 		plot_thread = new WaveformPlotThread(getHolder(), this);
 		
+		ch1_color.setColor(Color.YELLOW);
+		ch2_color.setColor(Color.RED);
 		grid_paint.setColor(Color.rgb(100, 100, 100));
 		cross_paint.setColor(Color.rgb(70, 100, 70));
 		outline_paint.setColor(Color.GREEN);
@@ -62,6 +85,27 @@ public class WaveformView extends SurfaceView implements SurfaceHolder.Callback{
 		PlotPoints(canvas);
 	}
 	
+	public void set_data(int[] data1, int[] data2 ){
+		
+		plot_thread.setRunning(false);
+		
+		for(int x=0; x<WIDTH; x++){
+			// channel 1
+			if(x<(data1.length)){
+				ch1_data[x] = HEIGHT-data1[x]+1;
+			}else{
+				ch1_data[x] = ch1_pos;
+			}
+			// channel 2
+			if(x<(data1.length)){
+				ch2_data[x] = HEIGHT-data2[x]+1;
+			}else{
+				ch2_data[x] = ch2_pos;
+			}
+		}
+		plot_thread.setRunning(true);
+	}
+	
 	public void PlotPoints(Canvas canvas){
 		// clear screen
 		canvas.drawColor(Color.rgb(20, 20, 20));
@@ -85,6 +129,10 @@ public class WaveformView extends SurfaceView implements SurfaceHolder.Callback{
  		canvas.drawLine(0, (HEIGHT+1), (WIDTH+1), (HEIGHT+1), outline_paint); // bottom
  		canvas.drawLine(0, 0, 0, (HEIGHT+1), outline_paint); //left
  		
- 		// TODO: actual plotting of points
+ 		// plot data
+		for(int x=0; x<(WIDTH-1); x++){			
+			canvas.drawLine(x+1, ch2_data[x], x+2, ch2_data[x+1], ch2_color);
+			canvas.drawLine(x+1, ch1_data[x], x+2, ch1_data[x+1], ch1_color);
+		}
 	}
 }
