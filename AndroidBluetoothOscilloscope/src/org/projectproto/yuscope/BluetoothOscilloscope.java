486bc7e935ef2b8df554083bba56bc492ec9dbad
yus <yus.projectproto@gmail.com>
2011-09-11 09:58:43 +0800

    added routine for enabling Bluetooth hardware and calling the DeviceListActivity

--- AndroidBluetoothOscilloscope/src/org/projectproto/yuscope/BluetoothOscilloscope.java
+++ AndroidBluetoothOscilloscope/src/org/projectproto/yuscope/BluetoothOscilloscope.java
@@ -1,10 +1,16 @@
 package org.projectproto.yuscope;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class BluetoothOscilloscope extends Activity {
+public class BluetoothOscilloscope extends Activity implements  Button.OnClickListener{
 	
 	// Message types sent from the BluetoothRfcommClient Handler
     public static final int MESSAGE_STATE_CHANGE = 1;
@@ -17,6 +23,17 @@ public class BluetoothOscilloscope extends Activity {
     public static final String DEVICE_NAME = "device_name";
     public static final String TOAST = "toast";
     
+    // Intent request codes
+    private static final int REQUEST_CONNECT_DEVICE = 1;
+    private static final int REQUEST_ENABLE_BT = 2;
+    
+    // Layout Views
+    private Button mConnectButton;
+    
+    // Local Bluetooth adapter
+    private BluetoothAdapter mBluetoothAdapter = null;
+    
+    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,5 +42,65 @@ public class BluetoothOscilloscope extends Activity {
         // Set up the window layout
         requestWindowFeature(Window.FEATURE_NO_TITLE);        
         setContentView(R.layout.main);
+        
+        // Get local Bluetooth adapter
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        
+        // If the adapter is null, then Bluetooth is not supported
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
+            finish();
+            return;
+        }
+    }
+    
+    @Override
+    public void onStart(){
+    	super.onStart();
+    	
+    	// If BT is not on, request that it be enabled.
+    	if (!mBluetoothAdapter.isEnabled()){
+    		Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+    		startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+    	}
+    	// Otherwise, setup the Oscillosope session
+    	else{
+    		setupOscilloscope();
+    	}    	
+    }
+    
+    @Override
+    public synchronized void onResume(){
+    	super.onResume();
+    }
+    
+    @Override
+    public void  onClick(View v){
+    	int buttonID;
+    	buttonID = v.getId();
+    	switch (buttonID){
+    	default:
+    		break;
+    	}
+    }
+    
+    @Override
+    public void onDestroy(){
+    	super.onDestroy();
+    }
+    
+    private void setupOscilloscope(){
+    	
+    	mConnectButton = (Button) findViewById(R.id.button_connect);
+    	mConnectButton.setOnClickListener(new OnClickListener(){
+			public void onClick(View arg0) {
+				BTConnect();
+			}    		
+    	});
+    }
+    
+    private void BTConnect(){
+    	Intent serverIntent = new Intent(this, DeviceListActivity.class);
+    	startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE);
     }
 }
--- AndroidBluetoothOscilloscope/src/org/projectproto/yuscope/DeviceListActivity.java
+++ AndroidBluetoothOscilloscope/src/org/projectproto/yuscope/DeviceListActivity.java
@@ -1,7 +1,42 @@
 package org.projectproto.yuscope;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class DeviceListActivity extends Activity {
-
+	// Return Intent extra
+    public static String EXTRA_DEVICE_ADDRESS = "device_address";
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+    	super.onCreate(savedInstanceState);
+    	
+    	// Setup the window
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
+        setContentView(R.layout.device_list);
+        
+        // Set result CANCELED incase the user backs out
+        setResult(Activity.RESULT_CANCELED);
+        
+        // Initialize the button to perform device discovery
+        Button scanButton = (Button) findViewById(R.id.button_scan);
+        scanButton.setOnClickListener(new OnClickListener(){
+        	public void onClick(View v){
+        		doDiscovery();
+        		v.setVisibility(View.GONE);
+        	}
+        });
+    }
+    
+    /**
+     * Start device discover with the BluetoothAdapter
+     */
+    private void doDiscovery(){
+    	Toast.makeText(this, "todo: discovering remote devices", Toast.LENGTH_LONG).show();
+    }
 }
